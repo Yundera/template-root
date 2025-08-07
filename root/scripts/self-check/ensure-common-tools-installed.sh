4e4800497c2aef4e8412a08a87044207058b941e
Pierre <worph@users.noreply.github.com>
2025-08-07 15:32:39 +0200

    add new common tools

--- root/scripts/self-check/ensure-common-tools-installed.sh
+++ root/scripts/self-check/ensure-common-tools-installed.sh
@@ -2,6 +2,6 @@
 # Script to ensure common tools are installed
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get install -qq -y htop isc-dhcp-client
+apt-get install -qq -y wget unzip rsync htop isc-dhcp-client
 
 echo "Common tools are installed"
